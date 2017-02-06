c05bb6e6e7033a4de9ad0c4a46878a501fb4ba51
CeliaChen0906 <zc2320@columbia.edu>
2017-02-06 16:24:22 +0000

    testrun1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
 import numpy as np
+from scripts import sf
 
 app = Flask(__name__)
+app.vars = {}
 
 @app.route('/')
 def main():
@@ -11,43 +13,24 @@ def main():
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template(
+            'index.html')
     
     else:  
-        #read data for sf and nyc
-        sfdfc = pd.read_csv('data/sfdfc.csv', index_col = 0)
-        nydfc = pd.read_csv('data/nydfc.csv', index_col = 0)
-        #get coordinates
-        sflat = request.args.get('lat')
-        sflong = request.args.get('long')
-        sf_distance = dict()
-        ny_rec = dict()
-        for i in range(len(sfdfc)):
-            sf_distance[i] = np.sqrt((sflat - sfdfc['INTPTLAT'][i])**2 + (sflong - sfdfc['INTPTLONG                                                                                                                  '][i])**2)
-        for index, v in sf_distance.iteritems():
-            if v == min(sf_distance.values()):
-                j = index
-        #how many recommendation you want?
-        n = 20
-        sf_l = sfdfc['liquordensereal'][j:j+1].values[0]
-        sf_p = sfdfc['popdensereal'][j:j+1].values[0]
-        sf_h = sfdfc['housedensereal'][j:j+1].values[0]
-        for q in range(len(nydfc)):
-            ny_rec[q] = (np.sqrt((sf_l - nydfc['liquordensereal'][q])**2)
-                         + np.sqrt((sf_p - nydfc['popdensereal'][q])**2) +
-                        np.sqrt((sf_h - nydfc['housedensereal'][q])**2))
-        res = sorted(ny_rec.items(), key = lambda x:x[1])
-        res_coor = []
-        for c in range(n):
-            index = res[:n][c][0]
-            lat = nydfc['INTPTLAT'][index]
-            lon = nydfc['INTPTLONG                                                                                                                  '][index]
-            if lat < 40.89 and lon > -74.049835 and lon < -73.81:
-                co = (lat, lon)
-                res_coor.append(co)
-        return render_template('test.html', rec = res_coor)
+        app.vars['lat'] = float(request.form['lat'])
+        app.vars['long'] = float(request.form['long'])
+        return redirect('/recommendation')
+    
+@app.route('/recommendation', methods=['GET'])
+def recommendation():
+        
+        to_gmap = sf.recommendation_coor(app.vars['lat'], app.vars['long'])
 
+        return render_template(
+            'recommendation.html',
+            to_gmap=to_gmap
+        )
 if __name__ == '__main__':
-    debug = True
+    #debug = True
     app.run()
 #host='104.131.67.119'
---
+++ scripts/sf.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import math
+from collections import defaultdict
+from os.path import abspath, dirname, join
+
+_cwd = dirname(abspath(__file__))
+
+sfdfc = pd.read_csv(join(_cwd, '../data/sfdfc.csv'), index_col = 0)
+nydfc = pd.read_csv(join(_cwd, '../data/nydfc.csv'), index_col = 0)
+
+
+def recommendation_coor(sflat, sflong):
+#recommendation system
+#input:coordinates in SF
+#sflat = 37.776745
+#sflong = -122.416745
+#find FIPS
+    sf_distance = defaultdict(int)
+    ny_rec = defaultdict(int)
+    for i in range(len(sfdfc)):
+        sf_distance[i] = np.sqrt((sflat - sfdfc['INTPTLAT'][i])**2 + (sflong - sfdfc['INTPTLONG                                                                                                                  '][i])**2)
+    for index, v in sf_distance.iteritems():
+        if v == min(sf_distance.values()):
+            j = index
+#how many recommendation you want?
+    n = 20
+    sf_l = sfdfc['liquordensereal'][j:j+1].values[0]
+    sf_p = sfdfc['popdensereal'][j:j+1].values[0]
+    sf_h = sfdfc['housedensereal'][j:j+1].values[0]
+    for q in range(len(nydfc)):
+        ny_rec[q] = (np.sqrt((sf_l - nydfc['liquordensereal'][q])**2)
+         + np.sqrt((sf_p - nydfc['popdensereal'][q])**2) +
+        np.sqrt((sf_h - nydfc['housedensereal'][q])**2))
+    
+    res = sorted(ny_rec.items(), key = lambda x:x[1])
+
+    res_coor = []
+    for c in range(n):
+        index = res[:n][c][0]
+        lat = nydfc['INTPTLAT'][index]
+        lon = nydfc['INTPTLONG                                                                                                                  '][index]
+        if lat < 40.89 and lon > -74.049835 and lon < -73.81:
+            co = (lat, lon)
+            res_coor.append(co)
+    goog_map_points = []
+    for i in res_coor:
+        goog_map_points.append('new google.maps.LatLng'+str(i))
+    return goog_map_points
